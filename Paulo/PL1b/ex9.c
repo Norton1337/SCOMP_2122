9e7b0a744224433adec518f8421d7757d5a01e3b
PauloNorton <1210824@isep.ipp.pt>
2022-03-31 10:39:55 +0100

    exercise 9

---
+++ Paulo/PL1b/ex9.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wait.h>
+#include <time.h>
+
+volatile sig_atomic_t test=0;
+
+void handle_signal(int signo, siginfo_t *sinfo, void *context)
+{
+	test++;
+}
+
+
+
+int main() {
+	
+	pid_t child=fork();
+	
+	if(child==0){
+		struct sigaction act;
+		memset(&act, 0, sizeof(struct sigaction));
+		
+		sigfillset(&act.sa_mask); 
+		sigdelset(&act.sa_mask, SIGUSR1);
+		
+		act.sa_sigaction = handle_signal;
+		sigaction(SIGUSR1, &act, NULL);
+		
+		int num = (rand() % (5 - 1 + 1)) + 1;
+		printf("Executing task B for %d seconds\n", num);
+		for(time_t start2 = time(NULL);time(NULL)-start2 < num;){
+			printf("Task B\n");
+			sleep(1);
+			
+		}
+		
+		
+		while(test==0){}
+		printf("Task C!\n");
+		exit(0);
+		
+	}
+	printf("child: %d\n",child);
+	printf("Executing task A\n");
+	for(time_t start = time(NULL);time(NULL)-start < 3;)
+	{
+		
+		printf("%ld seconds have passed.\n",time(NULL)-start);
+		sleep(1);
+	}
+	kill(child, 10);
+	
+}
