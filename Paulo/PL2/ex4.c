5ff2982ba2e5d905c9dc18d3d2fab5edd1787cf4
PauloNorton <1210824@isep.ipp.pt>
2022-03-31 18:20:12 +0100

    Fich PL2a até ex5

---
+++ Paulo/PL2/ex4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <wait.h>
+
+int main(void){
+
+	int fd[2];
+	pid_t pid;
+	if(pipe(fd) == -1){
+		perror("Pipe failed");
+		return 1;
+	}
+	pid = fork();
+	if(pid > 0){
+		// Parent
+		close(fd[0]);
+		
+		char line[256];
+		FILE *fp = fopen ("ex4text.txt", "r"); 
+		int linesAmount =0;
+		while(fgets(line, sizeof(line), fp)) {
+			linesAmount++;
+		} 
+		fclose(fp);
+		write(fd[1],&linesAmount,sizeof(int));
+		fp = fopen ("ex4text.txt", "r"); 
+		while(fgets(line, sizeof(line), fp)) {
+			 write(fd[1],line,sizeof(line));
+		} 
+		fclose(fp); 
+
+		close(fd[1]);
+		int status=0;
+		wait(&status);
+		printf("\n\nChild has finished\n");
+
+	} else{
+		// Child
+		close(fd[1]);
+		int amountLines;
+		char string[256];
+		read(fd[0],&amountLines,sizeof(int));
+		printf("\n");
+		for(int i=0;i<amountLines;i++){
+			read(fd[0],string,sizeof(string));
+			printf("%s",string);
+		}
+		close(fd[0]);
+		exit(1);
+		}
+	
+}
